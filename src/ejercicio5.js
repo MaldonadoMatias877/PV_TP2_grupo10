e7ec3a1bededb753ff4d4e52699ff207028e5ee6
Ricardo Condori <ricardocondori485@gmail.com>
2025-04-14 08:44:20 -0300

    Agrego ejercicios 5, 10 y 15

---
+++ src/ejercicio5.js
@@ -0,0 +1,6 @@
+// ejercicio5.js - Conversión de horas/minutos a segundos
+const horas = 2;
+const minutos = 30;
+const segundos = (horas * 3600) + (minutos * 60);
+
+console.log(`${horas}h ${minutos}min = ${segundos} segundos`);
